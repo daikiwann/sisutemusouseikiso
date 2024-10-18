24b55087b836c366ad6115b713080c0147e63d49
daikiwann <t236014@st.yamagata-u.ac.jp>
2024-10-18 19:21:23 +0900

    Add files via upload

---
+++ apples2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+ int k, j, s;
+	
+ printf("How many apples?");
+ scanf("%d",&k);
+	
+	printf("How many bananas?");
+	scanf("%d",&j);
+	
+	s=k+j;
+	
+ printf("The number of apples and bananas are %d",s);
+return 0;
+ }
